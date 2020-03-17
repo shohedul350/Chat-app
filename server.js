50d0901bc10149c8d41aa39522bb8aa04eaf2b53
shohedul350 <shohedulislamemon@gmail.com>
2020-03-17 19:25:25 +0600

    connect user system

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const router = require('./routes/route');
+const {
+  addUser, removeUser, getUser, getUserInroom,
+} = require('./user/user');
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,9 +15,19 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 io.on('connection', (socket) => {
-  console.log('we have a new connection');
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the chatbox ${user.room}` });
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, hass joined` });
+    socket.join(user.room);
+    callback();
+    return 0;
+  });
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit('message', { user: user.name, text: message });
+    callback();
   });
   socket.on('disconnect', () => {
     console.log('user had left');
