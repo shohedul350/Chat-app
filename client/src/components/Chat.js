97de5e9e53bbc18dab2f6cf5a9a41d500a6f49c3
shohedul350 <shohedulislamemon@gmail.com>
2020-03-17 17:56:17 +0600

    modified

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -13,7 +13,14 @@ let socket;
         socket = io(ENDPOINT);
         setName(name);
         setRoom(room)
-        socket.emit('join', {name,room})
+        socket.emit('join', {name,room}, ()=>{
+
+        });
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
+
     },[ENDPOINT, location.search])
     return (
         <div>
